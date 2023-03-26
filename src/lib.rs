423a760ec653c7c6418ef8f25b402a6bdec34682
lkbhargav <lkbhargav9@gmail.com>
2023-03-26 18:31:01 -0400

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+use anyhow::Result;
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
+
+pub enum Relay {
+    Gmail,
+}
+
+pub struct Email<'a> {
+    from: &'a str,
+    reply_to: &'a str,
+    mailer: SmtpTransport,
+}
+
+impl<'a> Email<'a> {
+    pub fn new(
+        from: &'a str,
+        reply_to: &'a str,
+        username: &str,
+        app_password: &str,
+        relay: Relay,
+    ) -> Result<Email<'a>> {
+        let creds = Credentials::new(username.to_string(), app_password.to_string());
+
+        let relay = match relay {
+            Relay::Gmail => "smtp.gmail.com",
+        };
+
+        // Open a remote connection to gmail
+        let mailer = SmtpTransport::relay(relay)?.credentials(creds).build();
+
+        Ok(Email {
+            from,
+            reply_to,
+            mailer,
+        })
+    }
+
+    pub fn send(&self, to: &str, subject: &str, body: &str) -> Result<()> {
+        let header = ContentType::TEXT_PLAIN;
+
+        let email = Message::builder()
+            .from(self.from.parse()?)
+            .reply_to(self.reply_to.parse()?)
+            .header(header)
+            .to(to.parse()?)
+            .subject(subject)
+            .body(String::from(body))?;
+
+        let _ = &self.mailer.send(&email)?;
+
+        Ok(())
+    }
+}
